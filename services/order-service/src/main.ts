695f06f30f9c72870f7b4e078b39e927a9d5a312
sreeragpariyarath <psreerag69@gmail.com>
2026-08-02 00:26:58 +0530

    fix: update CORS origins config to allow requests from the admin portal http://localhost:3101

--- services/order-service/src/main.ts
+++ services/order-service/src/main.ts
@@ -7,7 +7,10 @@ import { setupSwagger } from './swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: process.env.FRONTEND_URL ?? 'http://localhost:3100',
+    origin: [
+      process.env.FRONTEND_URL ?? 'http://localhost:3100',
+      'http://localhost:3101',
+    ],
   });
   app.useGlobalPipes(
     new ValidationPipe({
